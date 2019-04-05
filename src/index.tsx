f733a913b190acfec14d9bb98b050ed46c16a63c
Casper Engelmann <cme@web-koncept.dk>
2019-04-05 11:24:22 +0200

    changed root target

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,7 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('beregner'));
+ReactDOM.render(<App />, document.getElementById('roof-beregner'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
