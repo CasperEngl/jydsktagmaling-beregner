fe0ec9289b8e40e8481d7be6e71eb345c1a53077
Casper Engelmann <cme@web-koncept.dk>
2019-04-05 11:27:57 +0200

    added check for root element

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,9 @@ import './index.scss';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('roof-beregner'));
+if (document.getElementById('roof-beregner')) {
+  ReactDOM.render(<App />, document.getElementById('roof-beregner'));
+}
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
